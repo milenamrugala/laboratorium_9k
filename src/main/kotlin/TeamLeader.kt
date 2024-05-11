ea46d1cc1da0cddf200415521764242d7d5ece07
milenamrugala <milena.mrugala@icloud.com>
2024-05-11 19:58:35 +0200

    first commit of classes

--- src/main/kotlin/TeamLeader.kt
+++ src/main/kotlin/TeamLeader.kt
@@ -1,4 +1,5 @@
-class Leader(
+// klasa Kierownik
+open class TeamLeader(
     firstName: String,
     lastName: String,
     salary: Double,
@@ -6,6 +7,8 @@ class Leader(
     private var department: String,
     private var bonus: Double
 ) : PermanentEmployee(firstName, lastName, salary, contract) {
+    constructor(firstName: String, lastName: String, contract: Contract, department: String, bonus: Double) :
+            this(firstName, lastName, 0.0, contract, department, bonus)
     override fun toString(): String {
         return "${super.toString()}, Department: $department, Bonus: $bonus"
     }
