33d9edb0f05897b8990be77512cd44fab145ddd4
milenamrugala <milena.mrugala@icloud.com>
2024-05-11 19:58:18 +0200

    first commit of classes

--- src/main/kotlin/Head.kt
+++ src/main/kotlin/Head.kt
@@ -1,2 +1,11 @@
-class Head {
+// klasa Dyrektor
+class Head(
+    firstName: String,
+    lastName: String,
+    salary: Double,
+    contract: Contract,
+    department: String,
+    bonus: Double
+) : TeamLeader(firstName, lastName, salary, contract, department, bonus) {
+    constructor(firstName: String, lastName: String, contract: Contract, department: String) : this(firstName, lastName, 0.0, contract, department, 0.0)
 }
