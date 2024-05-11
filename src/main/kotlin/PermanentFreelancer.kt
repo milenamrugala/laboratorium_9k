c7d304900029b8b40e74d024cc578cfef0edff38
milenamrugala <milena.mrugala@icloud.com>
2024-05-11 19:58:32 +0200

    first commit of classes

--- src/main/kotlin/PermanentFreelancer.kt
+++ src/main/kotlin/PermanentFreelancer.kt
@@ -1,2 +1,8 @@
-class PermanentFreelancer {
+class PermanentFreelancer(
+    firstName: String,
+    lastName: String,
+    salary: Double,
+    contract: Contract
+) : Freelancer(firstName, lastName, salary, contract) {
+    constructor(firstName: String, lastName: String, contract: Contract) : this(firstName, lastName, 0.0, contract)
 }
