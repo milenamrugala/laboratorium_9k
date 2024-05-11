f61af06ca51688ac77f79884f4805f28fa6ca450
milenamrugala <milena.mrugala@icloud.com>
2024-05-11 19:58:16 +0200

    first commit of classes

--- src/main/kotlin/Freelancer.kt
+++ src/main/kotlin/Freelancer.kt
@@ -1,2 +1,11 @@
-class Freelancer {
+open class Freelancer(
+    firstName: String,
+    lastName: String,
+    salary: Double,
+    private var contract: Contract
+) : Employee(firstName, lastName, salary) {
+    constructor(firstName: String, lastName: String, contract: Contract) : this(firstName, lastName, 0.0, contract)
+    override fun toString(): String {
+        return "${super.toString()}, Contract: $contract"
+    }
 }
