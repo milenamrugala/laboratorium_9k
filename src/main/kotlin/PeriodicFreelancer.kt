0ffe735e57abd50ed833285831b51a60371732cc
milenamrugala <milena.mrugala@icloud.com>
2024-05-11 19:58:25 +0200

    first commit of classes

--- src/main/kotlin/PeriodicFreelancer.kt
+++ src/main/kotlin/PeriodicFreelancer.kt
@@ -1,2 +1,12 @@
-class PeriodicFreelancer {
+class PeriodicFreelancer (
+    firstName: String,
+    lastName: String,
+    salary: Double,
+    contract: Contract,
+    private var period: Int
+) : Freelancer(firstName, lastName, salary, contract) {
+    constructor(firstName: String, lastName: String, contract: Contract, period: Int) : this(firstName, lastName, 0.0, contract, period)
+    override fun toString(): String {
+        return "${super.toString()}, Period: $period"
+    }
 }
