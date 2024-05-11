241233a4f27c6a8ea26f133f042e441fa39eb228
milenamrugala <milena.mrugala@icloud.com>
2024-05-11 19:58:13 +0200

    first commit of classes

--- src/main/kotlin/Employee.kt
+++ src/main/kotlin/Employee.kt
@@ -1,2 +1,35 @@
-class Employee {
+open class Employee(
+    private var firstName: String,
+    private var lastName: String,
+    private var salary: Double
+) {
+
+    constructor(firstName: String, lastName: String) : this(firstName, lastName, 0.0)
+
+    override fun toString(): String {
+        return "Class: ${this.javaClass.simpleName}, Name: $firstName $lastName, Salary: $salary"
+    }
+
+    fun getFirstName(): String {
+        return firstName
+    }
+
+    fun getLastName(): String {
+        return lastName
+    }
+
+    fun getSalary(): Double {
+        return salary
+    }
+    fun setFirstName(firstName: String) {
+        this.firstName = firstName
+    }
+
+    fun setLastName(lastName: String) {
+        this.lastName = lastName
+    }
+
+    fun setSalary(salary: Double) {
+        this.salary = salary
+    }
 }
