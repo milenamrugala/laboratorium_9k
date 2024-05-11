869771790717f9bd66350c656876134806a85b50
milenamrugala <milena.mrugala@icloud.com>
2024-05-11 19:58:10 +0200

    first commit of classes

--- src/main/kotlin/Contract.kt
+++ src/main/kotlin/Contract.kt
@@ -1,2 +1,15 @@
-class Contract {
+open class Contract(
+    private var type: String
+) {
+    override fun toString(): String {
+        return "Class: ${this.javaClass.simpleName}, Type: $type"
+    }
+
+    fun getType(): String {
+        return type
+    }
+
+    fun setType(firstName: String) {
+        this.type = type
+    }
 }
