e67639173a5fde7b913603c0d848f51669724375
milenamrugala <milena.mrugala@icloud.com>
2024-05-11 19:58:28 +0200

    first commit of classes

--- src/main/kotlin/PermanentEmployee.kt
+++ src/main/kotlin/PermanentEmployee.kt
@@ -1,2 +1,11 @@
-class PermamentEmployee {
+open class PermanentEmployee(
+    firstName: String,
+    lastName: String,
+    salary: Double,
+    private var contract: Contract
+) : Employee(firstName, lastName, salary){
+    constructor(firstName: String, lastName: String, contract: Contract) : this(firstName, lastName, 0.0, contract)
+    override fun toString(): String {
+        return "${super.toString()}, Contract: $contract"
+    }
 }
